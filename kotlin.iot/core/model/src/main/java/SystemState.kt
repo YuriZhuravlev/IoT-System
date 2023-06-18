58fbf1c18c4915fceba742438d0cc6f7450bd81d
zhuravlevyuri <zhuravlevyuri2000@gmail.com>
2023-06-18 16:46:09 +0300

    create controller

--- kotlin.iot/core/model/src/main/java/SystemState.kt
+++ kotlin.iot/core/model/src/main/java/SystemState.kt
@@ -4,5 +4,18 @@ data class SystemState(
         val temperature: Temperature,
         val waterLevel: WaterLevel,
         val pir: PassiveInfraredSensor,
-        val configurationTemperature: ConfigurationTemperature
-)
+        val configurationTemperature: ConfigurationTemperature,
+        val temperatureIndex: Int
+) {
+    companion object {
+        const val INIT_VALUE = -1
+
+        val INIT = SystemState(
+                Temperature(INIT_VALUE),
+                WaterLevel(INIT_VALUE),
+                PassiveInfraredSensor(false),
+                ConfigurationTemperature(listOf()),
+                INIT_VALUE
+        )
+    }
+}
--- kotlin.iot/core/model/src/main/java/Temperature.kt
+++ kotlin.iot/core/model/src/main/java/Temperature.kt
@@ -1,3 +1,6 @@
 package ru.zhuravlev.yuri.core.model
 
-data class Temperature(val value: Int)
+data class Temperature(val value: Int) {
+    operator fun compareTo(temperature: Temperature): Int =
+            value.compareTo(temperature.value)
+}
