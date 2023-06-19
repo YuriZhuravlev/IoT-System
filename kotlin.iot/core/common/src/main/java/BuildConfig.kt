f1fec2640b8372e0148a1f2fdb36dd31d2866d94
zhuravlevyuri <zhuravlevyuri2000@gmail.com>
2023-06-19 23:14:17 +0300

    Fix BuildConfig

--- kotlin.iot/core/common/src/main/java/BuildConfig.kt
+++ kotlin.iot/core/common/src/main/java/BuildConfig.kt
@@ -1,11 +1,16 @@
 package ru.zhuravlev.yuri.core
 
+import java.io.File
+import java.util.*
+
 object BuildConfig {
     private val properties by lazy {
-        System.getProperties()
+        Properties().apply { load(File(PROPERTIES_PATH).reader()) }
     }
 
     fun getString(name: String): String? = kotlin.runCatching {
         properties.getProperty(name) as? String
     }.getOrNull()
+
+    private const val PROPERTIES_PATH = "kotlin.iot/local.properties"
 }
